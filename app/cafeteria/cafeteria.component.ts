1df4af02807215da85ee6ce62a87d64cb31ba6c0
apael1993 <anushavan1993@gmail.com>
2017-03-17 21:20:41 +0400

    Navigation tabs implementation

--- app/cafeteria/cafeteria.component.ts
+++ app/cafeteria/cafeteria.component.ts
@@ -2,7 +2,6 @@
  * Created by Anushavan on 3/17/17.
  */
 import {Component} from "@angular/core";
-import {Router, ActivatedRoute} from "@angular/router";
 
 @Component({
     moduleId: module.id,
@@ -12,12 +11,7 @@ import {Router, ActivatedRoute} from "@angular/router";
 })
 
 export class CafeteriaComponent {
-
-    constructor(private router: Router,
-                private route: ActivatedRoute) { }
-
-    private navigateTo(path: string) {
-        this.router.navigate([path], {relativeTo: this.route});
-    }
     
+    constructor() { }
+
 }
